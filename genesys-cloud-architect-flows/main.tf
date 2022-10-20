8423a8a25ae277fda35cb42dd8f2194c3c3ae955
anushkasathe <anushka.sathe@genesys.com>
2022-10-20 15:16:54 -0400

    remove flows

--- genesys-cloud-architect-flows/main.tf
+++ genesys-cloud-architect-flows/main.tf
@@ -23,13 +23,3 @@ provider "genesyscloud" {
 }
 
 
-resource "genesyscloud_flow" "flow" {  
-filepath = "https://bitbucket.org/anushkasathe/automatedflows/raw/502853c812fd185b6fbe1aa8a634dde2e3c2387a/Archy%20Hello%20World_v1-0.yaml"
-}
-
-resource "genesyscloud_flow" "flow1" {  
-filepath = "https://bitbucket.org/anushkasathe/automatedflows/raw/502853c812fd185b6fbe1aa8a634dde2e3c2387a/JSON_FLOW_1.yaml"
-  substitutions = {
-   person_name = "add a person name here"
- }   
-}
